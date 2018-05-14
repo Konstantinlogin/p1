06ea76dac783edbb75f5e0b54be83adc2d7d01b9
Константин Елагин <konstantinelagin@MacBook-Air-Konstantin.local>
2018-05-14 22:52:37 +0300

    add fonts. icons. images. desktop header.

--- webpack.config.js
+++ webpack.config.js
@@ -144,10 +144,10 @@ let baseConfig = {
             })
         }, {
             test: /\.(jpe?g|png|gif|svg)$/i,
-            use: ['file-loader?name=/images/[name].[ext]']
+            use: ['file-loader?name=../images/[name].[ext]']
         }, {
             test: /\.(ttf|eot|woff|woff2)$/,
-            use: ['file-loader?name=/fonts/[name].[ext]']
+            use: ['file-loader?name=../fonts/[name].[ext]']
         }]
     },
     plugins: plugins
