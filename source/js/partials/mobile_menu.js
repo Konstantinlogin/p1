69dc92b2320c009850a782646add7ea61a6ce9c8
Константин Елагин <konstantinelagin@MacBook-Air-Konstantin.local>
2018-05-27 14:49:14 +0300

    fix anchors

--- source/js/partials/mobile_menu.js
+++ source/js/partials/mobile_menu.js
@@ -12,7 +12,8 @@ let onKeyDown = function (evt) {
 
 let onBodyClick = function (evt) {
     evt = evt || window.event;
-    if (evt.target.classList.contains('menu__link')) {
+    let targetHref = evt.target.getAttribute('href');
+    if (evt.target.classList.contains('menu__link') || /^#/.test(targetHref)) {
         closeMenu();
     }
 };
